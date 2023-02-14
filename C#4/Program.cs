4fbec3b9d59c8b1158a2b26cc6b122e16b3bb563
RomanZhukov-b <zukovroman232@gmail.com>
2023-02-14 21:44:32 +0300

    commit C#4

---
+++ C#4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает 
+//число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.Write("number:-> ");
+int N = int.Parse(Console.ReadLine());
+int a = 1;
+while (a<=N)
+{
+    Console.Write($"{a} ");
+    a++;
+}
