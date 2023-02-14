363a5d91f551e3b95a965806896004502082e75f
RomanZhukov-b <zukovroman232@gmail.com>
2023-02-14 21:30:00 +0300

    commit C#3

---
+++ C#3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+Console.Write("number:-> ");
+int number = int.Parse(Console.ReadLine());
+if (number%2 ==0)
+{
+    Console.WriteLine($"{number} четное");
+}
+else 
+{
+    Console.WriteLine($"{number} нечетное");
+}
+
+
+
+
+
