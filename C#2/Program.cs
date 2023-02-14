5ec9e08f25e6771872728e112698fd143ccf7912
RomanZhukov-b <zukovroman232@gmail.com>
2023-02-14 20:17:15 +0300

    commit C#2

---
+++ C#2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.Write("First number:-> ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Second number:-> ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Third number:-> ");
+int number3 = int.Parse(Console.ReadLine());
+
+   int result = number1;
+  if (number2 > result)
+  { 
+      result = number2;
+  }
+  if (number3 > result) 
+  {
+      result = number3;
+  }
+
+Console.WriteLine($"Максимальное чило:-> {result} ");
