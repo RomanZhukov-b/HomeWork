b86acc42dfba472a33cc667f379b9e2cc79c81b7
RomanZhukov-b <zukovroman232@gmail.com>
2023-02-14 19:05:46 +0300

    commit C#1

---
+++ C#_1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
+Console.WriteLine("First number:-> ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Second number:-> ");
+int number2 = int.Parse(Console.ReadLine());
+int max = number1;
+if (max < number2)
+    max = number2;
+Console.WriteLine($"Максимальная цифра:-> {max} ");
+
+    
+ 
