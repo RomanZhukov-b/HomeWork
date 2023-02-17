c7db8fa14a8d44a17cb0d16809e152b9cbcd1634
RomanZhukov-b <zukovroman232@gmail.com>
2023-02-17 14:56:39 +0300

    commit C#7

---
+++ C#7/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+Console.Clear();
+ int ThirdDigit(int number)
+        {
+            int result = -1;
+            if (number >= 100)
+            {
+                while (number > 999)
+                {
+                    number = number / 10;
+                }
+                result = number % 10;
+            }
+            return result; 
+        }
+Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (ThirdDigit(number1) == -1)
+Console.WriteLine("третьей цифры нет");
+else
+Console.WriteLine($"Третье цифра {ThirdDigit(number1)}");
