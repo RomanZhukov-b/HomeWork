66d1ea3b1c6a7ff09760556b5bd861144306f26a
RomanZhukov-b <zukovroman232@gmail.com>
2023-02-21 17:47:58 +0300

    commit C#12

---
+++ C#12/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+int number = ReadInt("Введите число N: ");
+
+for (int i = 2; i <= number; i++)
+{ 
+    Console.Write($"{Math.Pow(i, 3)} ");
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
