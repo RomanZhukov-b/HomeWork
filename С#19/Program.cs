b372ca3b345d713689cd8227e3c955b56ba0e731
RomanZhukov-b <zukovroman232@gmail.com>
2023-03-02 17:19:05 +0300

    commit С#19

---
+++ С#19/Program.cs
@@ -0,0 +1,18 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел
+//  больше 0 ввёл пользователь.
+Console.Clear();
+
+Console.Write("Введите числа через пробел:-> ");
+int[] el = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int countFind(int[] el)
+{
+int count = 0;
+for(int i = 0; i < el.Length; i++)
+{
+if(el[i] > 0)
+count++;
+}
+return count;
+}
+Console.WriteLine($"Чисел больше 0:-> {countFind(el)}");
